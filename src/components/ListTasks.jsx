2222cf2da4f87094152040ddc542ee6d367936a1
Laos22 <olega1987@gmail.com>
2026-04-21 22:12:24 +0300

    feat: изменить минимальную ширину компонента ListTasks

--- src/components/ListTasks.jsx
+++ src/components/ListTasks.jsx
@@ -14,7 +14,7 @@ const ListTasks = ({ list }) => {
 
 
     return (
-        <div key={list.id} className="m-auto w-full min-w-128  max-w-192 h-full bg-white flex flex-col">
+        <div key={list.id} className="m-auto w-full min-w-98  max-w-192 h-full bg-white flex flex-col">
                 <div className="py-2 px-4 border-b border-gray-300 flex items-center justify-between">
                     <h1>{list.name}</h1>
                     <button
